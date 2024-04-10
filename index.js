fe208bf15865b80a81f8b69fb5e0fc9dfe699824
Tumaini <tumainikebaya.234@gmail.com>
2024-04-10 16:45:20 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(collection, name){
+    return collection.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(collection, letters){
+    return collection.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+function matchName(collection, name){
+    return collection.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
